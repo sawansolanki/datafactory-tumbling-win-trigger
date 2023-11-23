be7cd29991c9ac77a3291a0f3f5d94b32de73863
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-11-23 15:47:28 +0530

    Update change_time_tumbl_win_trig.sh

--- change_time_tumbl_win_trig.sh
+++ change_time_tumbl_win_trig.sh
@@ -8,7 +8,7 @@ json_file="pipeline.json"
 new_start_time=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 
 # Update the start time for the specified type in the JSON file
-jq --arg new_start_time "$new_start_time" '.resources |= map(if .type == "Microsoft.Synapse/workspaces/triggers" then .properties.typeProperties.startTime = $new_start_time else . end)' "$json_file" > temp.json \
+jq --arg new_start_time "$new_start_time" '.resources |= map(if .properties.type == "TumblingWindowTrigger" then .properties.typeProperties.startTime = $new_start_time else . end)' "$json_file" > temp.json \
 && mv temp.json "$json_file"
 
 echo "Start time updated successfully."
